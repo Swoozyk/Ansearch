80786dea45819cba763c1e22239f210128bde9dc
Aleksandr <aleksandr.vink@mail.ru>
2025-05-18 11:43:44 +0300

    fix allowed hosts

--- Ansearch/settings.py
+++ Ansearch/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 FILE_UPLOAD_PERMISSIONS = 0o644
 FILE_UPLOAD_HANDLERS = [
     'django.core.files.uploadhandler.MemoryFileUploadHandler',
